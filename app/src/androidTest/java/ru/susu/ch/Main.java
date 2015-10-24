c5a36d905abdb5c16dde738134a1d04f9d18767b
Alex_P <pekler-sasha@yandex.ru>
2015-10-24 23:03:32 +0500

    Android studio init
    Первый запуск Android Studio

---
+++ app/src/androidTest/java/ru/susu/ch/Main.java
@@ -0,0 +1,13 @@
+package ru.susu.ch;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class Main extends ApplicationTestCase<Application> {
+    public Main() {
+        super(Application.class);
+    }
+}
